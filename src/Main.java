041afb3d3067ffa0256517fcd547fb9cdf698ea8
Calyen93 <genco.claudio3@gmail.com>
2024-03-11 11:26:29 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Student> studentArrayList = new ArrayList<>();
+
+        studentArrayList.add(new Student("Claudio", 30));
+        studentArrayList.add(new Student("Carlo", 20));
+        studentArrayList.add(new Student("Claudia", 29));
+        studentArrayList.add(new Student("Marta", 31));
+
+        System.out.println("Lista degli studenti");
+        for (Student student : studentArrayList) {
+            System.out.println("Nome: " + student.getName() + ", Età: " + student.getAge());
+        }
+
+
+    }
+
+
+}
